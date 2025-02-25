64db30424f1d0d1aaa848a6a73ed2d49b3504437
Austin Tang <austin.tang@kellogg.northwestern.edu>
2025-02-25 03:06:48 +0000

    save 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root "places#index"
+
   # get("/", { :controller => "articles", :action => "index" })
+  resources :places, only: [:index, :show, :new, :create] do
+    resources :entries, only: [:new, :create]
+  end
 end
