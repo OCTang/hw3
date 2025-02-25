38036b91e260ab4ad205e8517907338d14e96c18
Austin Tang <austin.tang@kellogg.northwestern.edu>
2025-02-25 03:27:59 +0000

    save 3

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,22 @@
 class EntriesController < ApplicationController
+  def new
+    @place = Place.find(params[:place_id])
+    @entry = Entry.new
+  end
+
+  def create
+    @place = Place.find(params[:place_id])
+    @entry = @place.entries.new(entry_params)
+    if @entry.save
+      redirect_to place_path(@place), notice: "Entry added successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :description, :posted_on)
+  end
 end
